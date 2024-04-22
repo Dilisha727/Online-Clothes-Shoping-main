f81ec82590f4a2fcd73f50a5c75f97233fd7e3fb
dilisha <dilishapriyashan076@gmail.com>
2024-04-22 15:37:01 +0530

    Update Profile info

---
+++ Online Clothes Shoping/View/Home_page/Profile_Information.swift
@@ -0,0 +1,18 @@
+//
+//  Profile_Information.swift
+//  Online Clothes Shoping
+//
+//  Created by Dilisha priyashan on 2024-04-22.
+//
+
+import SwiftUI
+
+struct Profile_Information: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Profile_Information()
+}
